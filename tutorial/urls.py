f1cea8dc547dc4cfdcb94102f7d15fd02dd183f3
azizjon1103 <122139535+azizjon1103@users.noreply.github.com>
2023-01-21 18:09:59 +0500

    buldi

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from . import views
+from .views import HomePageView, AbautView, ServiseView, PriseView, BlogView, DetaillView, TeamView
+
 
 urlpatterns = [
-    path('', views.IndexView, name='home')
+    path('home/', HomePageView.as_view(), name = 'home'),
+    path('about/', AbautView.as_view(), name = 'about'),
+    path('servise/', ServiseView.as_view(), name = 'servise'),
+    path('prise/', PriseView.as_view(), name = 'prise'),
+    path('blok/', BlogView.as_view(), name = 'blok'),
+    path('dedail/', DetaillView.as_view(), name = 'dedail'),
+    path('deam/', TeamView.as_view(), name = 'deam' ),
 ]
