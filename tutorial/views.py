c5ef8c8340f5f5b1d60faa4ee5b08f06de57076e
azizjon1103 <122139535+azizjon1103@users.noreply.github.com>
2023-01-21 18:12:15 +0500

    yes

--- tutorial/views.py
+++ tutorial/views.py
@@ -1,6 +1,34 @@
+from django.views.generic import TemplateView
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+class HomePageView(TemplateView):
+    template_name = 'index.html'
 
-def IndexView(request):
-    return render(request,'index.html')
+
+class AbautView(TemplateView):
+    template_name = 'blog.html'
+
+
+class ServiseView(TemplateView):
+    template_name = 'contact.html'
+
+
+class PriseView(TemplateView):
+    template_name = 'portfolio.html'
+    
+
+
+class BlogView(TemplateView):
+    template_name = 'portfolio-item.html'
+
+
+class DetaillView(TemplateView):
+    template_name = 'ui-elements.html'
+
+
+class TeamView(TemplateView):
+    template_name = 'blog-post.html'
+
